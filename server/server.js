075212dd050d735c736bb56b73a29a6e4b47cfe4
Shruti270903 <shrutikashyap2709@gmail.com>
2025-07-31 11:54:47 +0530

    update

--- server/server.js
+++ server/server.js
@@ -4,26 +4,30 @@ import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 import mongoose from "mongoose";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 //Initialize Express
 const app = express();
-
-// mongoose.connect("mongodb+srv://shrutikashyap2709:A12bcdef@cluster0.pckfo8n.mongodb.net",{
-//   dbName:"llm",
-// }).then(()=>console.log("mongo db connected ")).catch((error)=>console.log("Error return: ",error))
 
 //Connect to db
 await connectDB()
 //middleweares
 app.use(cors())
+app.use(clerkMiddleware())
 
 //Route
 app.get('/', (req,res)=>res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
 
+
+app.use('/api/educator', express.json(), educatorRouter)
+
 //port
 const PORT =process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
-  console.log(`server is running on ${PORT}`)
+  console.log(`server is running on http://localhost:${PORT}`)
 })
+
+
