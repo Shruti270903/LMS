68fe3d25ddf7d8fecddd98949f093731d29103ec
Shruti270903 <shrutikashyap2709@gmail.com>
2025-07-31 07:29:22 +0530

    add changes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import mongoose from "mongoose";
 
 //Initialize Express
 const app = express();
+
+// mongoose.connect("mongodb+srv://shrutikashyap2709:A12bcdef@cluster0.pckfo8n.mongodb.net",{
+//   dbName:"llm",
+// }).then(()=>console.log("mongo db connected ")).catch((error)=>console.log("Error return: ",error))
 
 //Connect to db
 await connectDB()
