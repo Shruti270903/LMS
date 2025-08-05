3dadb52dae9e3f88357f5b26e3cb6a3aa35f1827
Shruti270903 <shrutikashyap2709@gmail.com>
2025-08-05 10:52:04 +0530

    updates

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
 
-//Connect to the MONGODB database
-
+// Connect to the MONGODB database
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  try {
+    mongoose.connection.on("connected", () => console.log("Database Connected"));
+    mongoose.connection.on("error", (err) => console.error("MongoDB connection error:", err));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/LMS`);
+    // Use the URI as-is (it should include the database name)
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1); // Exit process if unable to connect
+  }
 };
+
 export default connectDB;
