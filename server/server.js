741eb7a0008b76aa4a680137e9a83fa04ef69bc0
Shruti270903 <shrutikashyap2709@gmail.com>
2025-10-16 07:59:56 +0530

    commint

--- server/server.js
+++ server/server.js
@@ -1,29 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
 import User from './models/User.models.js';
 
-dotenv.config();
-
+//Initialize express app
 const app = express();
 
-// ✅ Middlewares
-app.use(cors());
-app.use(express.json()); // keep this ONCE globally
-
-// ✅ Connect MongoDB
+//connect to database
+app.use(express.json());
 await connectDB();
 
-// ✅ Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the LMS server! API working');
-});
 
-app.post('/clerk', clerkWebhooks);
+//Middleware
+app.use(cors());
 
-// // ✅ User creation route
+//Routes
+app.get('/', (req, res) =>  res.send('Welcome to the LMS server!  API working '));
+app.post('/clerk', express.json(), clerkWebhooks);
+
 // app.post('/user', async (req, res) => {
 //   try {
 //     const { _id, name, email, password, imageUrl } = req.body;
@@ -36,8 +32,9 @@ app.post('/clerk', clerkWebhooks);
 // });
 
 
-// ✅ Server
+//Port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
