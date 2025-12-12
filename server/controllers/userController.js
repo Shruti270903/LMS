1fe46e96ead1a08bd2933ca6e8962648c5c92c45
Shruti270903 <shrutikashyap2709@gmail.com>
2025-12-12 20:01:44 +0530

    commit changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,84 +1,98 @@
-import Stripe from "stripe"; 
+import Stripe from "stripe";
 import { Purchase } from "../models/Purchase.js";
-import User from '../models/User.models.js';
-import Course from '../models/Course.js';
+import User from "../models/User.models.js";
+import Course from "../models/Course.js";
 //Get User Data
-export const getUserData = async (req, res)=>{
-    try{
-        const userId = req.auth().userId
-        const user = await User.findById(userId)
+export const getUserData = async (req, res) => {
+  try {
+    const userId = req.auth().userId;
+    const user = await User.findById(userId);
 
-        if(!user){
-            return res.status(404).json({success: false, message: 'User Not Found'})
-        }
-        res.status(200).json({success:true, user})
-    } catch(error){
-      res.status(500).json({success: false, message: error.message})
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found" });
     }
-}
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 //Users Enrolled Course With Lecture Links
- export const userEnrolledCourses = async (req, res)=>{
-    try{
-        const userId = req.auth().userId
-        const user = await User.findById(userId).populate('enrolledCourses')
+export const userEnrolledCourses = async (req, res) => {
+  try {
+    const userId = req.auth().userId;
+    const user = await User.findById(userId).populate("enrolledCourses");
 
-        res.status(200).json({success:true, enrolledCourses: getUserData.enrolledCourses})
-    } catch(error){
-         res.status(500).json({success: false, message: 'user enrolled course issue'});
+    res
+      .status(200)
+      .json({ success: true, enrolledCourses: getUserData.enrolledCourses });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "user enrolled course issue" });
+  }
+};
+
+//Purchase course
+export const purchaseCourse = async (req, res) => {
+  try {
+    const { courseId } = req.body;
+    const { origin } = req.headers;
+    const userId = req.auth().userId;
+    const userData = await User.findById(userId);
+    const courseData = await Course.findById(courseId);
+    console.log("origin", origin);
+    console.log("courseId", courseId);
+    console.log("userId", userId);
+    console.log("UserData", userData);
+    console.log("courseData", courseData);
+
+    if (!userData || !courseData) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Data Not Found from purchase" });
     }
- }
 
- //Purchase course 
-export const purchaseCourse = async (req, res)=>{
+    const purchaseData = {
+      courseId: courseData._id,
+      userId,
+      amount:
+        courseData.coursePrice -
+        (courseData.discount * courseData.coursePrice) / 100,
+    };
+    const newPurchase = await Purchase.create(purchaseData);
+    //Stripe Gateway Initialize
+    const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-    try{
-        const {courseId}= req.body
-        const {origin} = req.headers
-        const userId = req.auth().userId
-        const userData = await User.findById(userId)
-        const courseData = await Course.findById(courseId)
+    const currency = process.env.CURRENCY.toLowerCase();
 
-        if(!userData || !courseData){
-            return res.status(404).json({success:false, message:'Data Not Found from purchase'})
-        }
+    //creating line items to for Stripe
 
-        const purchaseData = {
-            courseId : courseData._id,
-            userId,
-            amount:(courseData.coursePrice - (courseData.discount * courseData.coursePrice) / 100 ),
-        }
-        const newPurchase = await Purchase.create(purchaseData)
-        //Stripe Gateway Initialize
-        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
-        
-        const currency = process.env.CURRENCY.toLowerCase()
+    const line_items = [
+      {
+        price_data: {
+          currency,
+          product_data: {
+            name: courseData.courseTitle,
+          },
+          unit_amount: Math.floor(newPurchase.amount) * 100,
+        },
+        quantity: 1,
+      },
+    ];
 
-        //creating line items to for Stripe
-
-        const line_items=[{
-            price_data:{
-                currency,
-                product_data:{
-                    name: courseData.courseTitle
-                },
-                unit_amount: Math.floor(newPurchase.amount) * 100
-            },
-            quantity : 1
-        }]
-
-        const session = await stripeInstance.checkout.sessions.create({
-            success_url:`${origin}/loading/my-enrollments`,
-            cancel_url: `${origin}/`,
-            line_items: line_items,
-            mode: 'payment',
-            metadata:{
-                purchaseId: newPurchase._id.toString()
-            }
-        })
-res.status(200).json({ success: true, session_url: session.url });
-
-    }catch(error){
-        res.status(500).json({success: false, message:"stripe purchase error"})
-    }
-}
-
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-enrollments`,
+      cancel_url: `${origin}/`,
+      line_items: line_items,
+      mode: "payment",
+      metadata: {
+        purchaseId: newPurchase._id.toString(),
+      },
+    });
+    res.status(200).json({ success: true, session_url: session.url });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "stripe purchase error" });
+  }
+};
