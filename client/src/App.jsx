a0c56e50b269e47d44f564981812a3ab5124e3fc
Shruti270903 <shrutikashyap2709@gmail.com>
2025-12-30 23:24:20 +0530

    Fix Navbar import casing for Linux/Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Dashboard from "./pages/educator/Dashboard";
 import AddCourse from "./pages/educator/AddCourse";
 import MyCourses from "./pages/educator/MyCourses";
 import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
-import Navbar from "./components/student/navbar";
+import Navbar from "./components/student/Navbar";
 import "quill/dist/quill.snow.css";
 import { ToastContainer} from 'react-toastify';
 
