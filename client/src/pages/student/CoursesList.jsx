8801b3b0a65480a46041c8482c6d73b8a2304e83
Shruti270903 <shrutikashyap2709@gmail.com>
2025-12-30 18:33:42 +0530

    searchbars

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-import SearchBar from "../../components/student/searchBar";
+import SearchBar from "../../components/student/SearchBars";
 import { useParams } from "react-router-dom";
 import CourseCard from "../../components/student/courseCard";
 import { assets } from "../../assets/assets";
-import Footer from '../../components/student/Footer'
+import Footer from "../../components/student/Footer";
 
 const CoursesList = () => {
   const { navigate, allCourses } = useContext(AppContext);
   const { input } = useParams();
-  const [filteredCourse, setFilteredCourse] = useState([])
+  const [filteredCourse, setFilteredCourse] = useState([]);
 
-  useEffect(()=>{
-   if(allCourses && allCourses.length>0){
-    const tempCourses = allCourses.slice()
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
+      const tempCourses = allCourses.slice();
 
-    input ? 
-    setFilteredCourse(
-      tempCourses.filter(
-        item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
-      ))
-    : setFilteredCourse(tempCourses)
-   }
-  },[allCourses, input])
+      input
+        ? setFilteredCourse(
+            tempCourses.filter((item) =>
+              item.courseTitle.toLowerCase().includes(input.toLowerCase())
+            )
+          )
+        : setFilteredCourse(tempCourses);
+    }
+  }, [allCourses, input]);
   return (
     <>
       <div className="relative md:px-36 px-8 pt-20 text-left ">
@@ -44,19 +45,24 @@ const CoursesList = () => {
           </div>
           <SearchBar data={input} />
         </div>
-        {
-          input && <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8-mb-8 text-gray-600"> 
+        {input && (
+          <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8-mb-8 text-gray-600">
             <p>{input}</p>
-            <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={()=> navigate('/course-list')} />
-            </div>
-        }
+            <img
+              src={assets.cross_icon}
+              alt=""
+              className="cursor-pointer"
+              onClick={() => navigate("/course-list")}
+            />
+          </div>
+        )}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {filteredCourse.map((course, index) => (
             <CourseCard key={index} course={course} />
           ))}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
